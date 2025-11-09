5bc2a624ccfaa71eef0e27a2b688b0f1db49d24b
vis <v.vishal1102@gmail.com>
2025-11-09 12:33:32 +0530

    fixed mobile home button

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,9 +16,7 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center">
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
+            <div className="text-xl font-semibold sm:text-2xl">{siteMetadata.headerTitle}</div>
           ) : (
             siteMetadata.headerTitle
           )}
